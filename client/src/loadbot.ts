91bd2dae552636db8ca047cc47effabcbd1c7749
fabriciovo <fabricio.varisco@outlook.com>
2025-01-16 19:23:39 -0300

    fix loadbot ts

--- client/src/loadbot.ts
+++ client/src/loadbot.ts
@@ -1,20 +1,24 @@
 (function () {
-    const chatbotCssUrl = `${import.meta.env.VITE_CHATBOT_SITE}/assets/main.css`;
-    const chatbotJsUrl = `${import.meta.env.VITE_CHATBOT_SITE}/loadbot.js`;
-  
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = chatbotCssUrl;
-    link.media = "all";
-    document.head.appendChild(link);
-  
-    const chatbotContainer = document.createElement("div");
-    chatbotContainer.id = "chatbot";
-    document.body.appendChild(chatbotContainer);
-  
-    const script = document.createElement("script");
-    script.src = chatbotJsUrl;
-    script.async = true;
-    document.body.appendChild(script);
-  })();
-  
+  if (document.getElementById("chatbot")) {
+    console.warn("Chatbot já está carregado.");
+    return;
+  }
+
+  const chatbotCssUrl = `${import.meta.env.VITE_CHATBOT_SITE}/assets/main.css`;
+  const chatbotJsUrl = `${import.meta.env.VITE_CHATBOT_SITE}/loadbot.js`;
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = chatbotCssUrl;
+  link.media = "all";
+  document.head.appendChild(link);
+
+  const chatbotContainer = document.createElement("div");
+  chatbotContainer.id = "chatbot";
+  document.body.appendChild(chatbotContainer);
+
+  const script = document.createElement("script");
+  script.src = chatbotJsUrl;
+  script.async = true;
+  document.body.appendChild(script);
+})();
